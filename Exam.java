fefa55480a12ff13a3753a4d4022b460f3c9e13a
ishwaridataexpert8-tech <ishwaridataexpert8@gmail.com>
2026-06-20 23:50:35 +0530

    Add files via upload

---
+++ Exam.java
@@ -0,0 +1,36 @@
+
+public class Exam {
+public static void mian (String args[])
+{
+	1. int a = 10; a += 5; a -= 3; a *= 2; a /= 4; System.out.println(a);
+	2. int a = 5, b = 10, c = 15; System.out.println((a < b) && (b < c));
+	3. System.out.println((a > b) || (b < c)); System.out.println(!(a < c));
+	4. int x = 10, y = 20; System.out.println(x++ + ++y);
+	5. int a = 5, b = 2; System.out.println(a % b);
+	6. int p = 7, q = 3; System.out.println(p * q + p / q);
+	7. int n = 5; System.out.println(++n + n++);
+	8. int x = 5, y = 10; System.out.println(x > y ? x : y);
+	9. int a = 10, b = 5, c; c = a++ + --b + a * b; System.out.println(c);
+	10. int a = 4, b = 2; System.out.println(a << b);
+	11. int a = 8, b = 2; System.out.println(a >> b);
+	12. int a = 10; System.out.println(~a);
+	13. int a = 5, b = 7; System.out.println(a & b);
+	14. int a = 5, b = 7; System.out.println(a | b);
+	15. int a = 5, b = 7; System.out.println(a ^ b);
+	16. int a = 10; System.out.println(a++ + ++a + a--);
+	17. int a = 1; a += a++ + ++a; System.out.println(a);
+	18. int a = 5, b = 10; boolean result = (a < b) ? true : false; System.out.println(result);
+	19. int a = 5, b = 10; System.out.println(a == b);
+	20. int a = 3, b = 2; System.out.println(a * b + a / b);
+	21. int a = 5; a = a++ + ++a; System.out.println(a);
+	22. int a = 10, b = 5; System.out.println(a > b && a != b);
+	23. int x = 2, y = 3; x *= y + 1; System.out.println(x);
+	24. int a = 5; a += a -= a *= a; System.out.println(a);
+	25. int a = 4, b = 3; System.out.println(a > b ? a++ : ++b);
+	26. int a = 5; System.out.println(a++ + a++ + ++a);
+	27. int a = 2, b = 3; System.out.println(a + b * a / b);
+	28. int a = 10, b = 20; System.out.println(a < b && b > a);
+	29. int a = 6, b = 2; System.out.println((a / b) * (a % b));
+	30. int a = 5, b = 10, c = 15; System.out.println((a < b && b > c) || !(b < c));
+}
+}
